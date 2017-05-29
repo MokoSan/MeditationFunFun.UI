9b03f5aa86e0fd052d1e7c6b13e77ec277780b04
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-05-28 21:09:42 -0400

    POSTS working with Suave.

--- app/scripts/controllers/modalJournal.js
+++ app/scripts/controllers/modalJournal.js
@@ -8,7 +8,22 @@
  * Controller of the meditationFunFunApp
  */
 angular.module('meditationFunFunApp')
-    .controller('ModalJournalCtrl', function($scope, $uibModalInstance) {
+    .factory('JournalCreatorFactory', function($http){
+        var factory = {};
+        factory.create = function(scope, callback) {
+            var data = { "Title" : scope.title, "Content" : scope.content, "DateOfMeditation" : scope.creationDateTime }
+            var url = 'http://localhost:8080/api/v1/journals';
+            $http({
+                method: 'POST',
+                url: url,
+                data : data,
+                headers : {'Content-Type' : 'application/x-www-form-urlencoded'}
+            }).then(callback);
+        }
+
+        return factory;
+    })
+    .controller('ModalJournalCtrl', function($scope, $uibModalInstance, JournalCreatorFactory) {
 
         $scope.creationDateTime = new Date();
         var creationDate = $scope.creationDateTime.toLocaleDateString();
@@ -18,6 +33,10 @@ angular.module('meditationFunFunApp')
         $scope.content = "Describe your meditation in 200 characters.";
 
         $scope.save = function() {
+            JournalCreatorFactory.create($scope, function(data) {
+                console.log(data);
+            });
+
             $uibModalInstance.close();
         }
 
